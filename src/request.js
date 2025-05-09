f231ce3aac9b592a3e2e56090b479a1387accc22
Juan Mesaglio <juan.mesaglio@gmail.com>
2025-05-09 11:22:33 -0300

    feat(requests): add timeout policy
    Signed-off-by: Juan Mesaglio <juan.mesaglio@gmail.com>

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@ const http = require('http');
  * Alguien nos dejó esta implementación de un GET en node.
  * ¡No cambiar interfaz!
  */
-function get(servicio, ruta, cb) {
+function get(servicio, ruta, cb, timeout=2000) {
     const opciones = {
         hostname: servicio.host,
         port: servicio.puerto,
@@ -29,9 +29,16 @@ function get(servicio, ruta, cb) {
                 }
             });
     });
+
     req.on('error', error => {
         cb(error);
     });
+    
+    req.setTimeout(timeout, () => {
+        req.destroy();
+        cb(new Error(`Timeout al conectar con ${servicio.nombre}${ruta} después de ${timeout}ms`));
+    });
+    
     req.end();
 }
 
